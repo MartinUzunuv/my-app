fa2d983f0af19939331ff7dbc9b52cc0f5ba908c
MartinUzunuv <uzunovm0@gmail.com>
2022-10-15 02:52:34 +0300

    Made the canvas. Now to fix it ig...

--- src/components/JengaPart.js
+++ src/components/JengaPart.js
@@ -2,8 +2,27 @@ import React from 'react'
 import './JengaPart.css'
 
 const JengaPart = () => {
+    const onClick = () => {
+        var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d');
+      var centerX = canvas.width / 2;
+      var centerY = canvas.height / 2;
+      var radius = 70;
+
+      
+
+      context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = 'green';
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = '#003300';
+      context.stroke();
+    }
   return (
-    <div className='JengaPart'>JengaPart</div>
+    <div className='JengaPart'>
+        <canvas className='Canvas' id="myCanvas" width="500%" height="500%" onClick={onClick}></canvas>
+    </div>
   )
 }
 
