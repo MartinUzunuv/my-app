739c3af025a023b1713ccb789e7a0cdb7723ca9f
MartinUzunuv <uzunovm0@gmail.com>
2022-10-16 15:02:43 +0300

    UseEfect added

--- src/components/JengaPart.js
+++ src/components/JengaPart.js
@@ -1,27 +1,24 @@
-import React from 'react'
+import React, { useRef, useEffect  } from 'react'
 import './JengaPart.css'
 
 const JengaPart = () => {
-    const onClick = () => {
-        var canvas = document.getElementById('myCanvas');
-      var context = canvas.getContext('2d');
-      var centerX = canvas.width / 2;
-      var centerY = canvas.height / 2;
-      var radius = 70;
 
-      
 
-      context.beginPath();
-      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-      context.fillStyle = 'green';
-      context.fill();
-      context.lineWidth = 5;
-      context.strokeStyle = '#003300';
-      context.stroke();
-    }
+
+  const canvasRef = useRef(null)
+  
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    //Our first draw
+    context.fillStyle = '#000000'
+    context.fillRect(0, 0, context.canvas.width/2, context.canvas.height/2)
+  }, [])
+
+ 
   return (
     <div className='JengaPart'>
-        <canvas className='Canvas' id="myCanvas" width="500%" height="500%" onClick={onClick}></canvas>
+        <canvas ref={canvasRef} className='Canvas' id="myCanvas" width="500" height="500"></canvas>
     </div>
   )
 }
