744760aa571210191b2b8a80b13c789a4274ea51
MartinUzunuv <uzunovm0@gmail.com>
2022-10-20 00:33:08 +0300

    div is now a radio button

--- src/components/QuizPart.js
+++ src/components/QuizPart.js
@@ -30,53 +30,66 @@ const QuizPart = ({ setRemoveBlock, removeBlock }) => {
     },
   ];
 
-  const [question, setQuestion] = useState(questions[Math.floor(Math.random()*questions.length)])
+  const [question, setQuestion] = useState(
+    questions[Math.floor(Math.random() * questions.length)]
+  );
 
   const onSubmit = (e) => {
-    e.preventDefault()
-    var ans = document.getElementsByName('ans');
-    let isCorect = false
-    for(let i = 0; i < ans.length; i++){
-      if(ans[i].checked){
-        if(ans[i].value === question.t){
-          isCorect = true
+    e.preventDefault();
+    var ans = document.getElementsByName("ans");
+    let isCorect = false;
+    for (let i = 0; i < ans.length; i++) {
+      if (ans[i].checked) {
+        if (ans[i].value === question.t) {
+          isCorect = true;
         }
       }
     }
-    if(isCorect === true){
-      console.log("yes")
-    }else{
-      console.log("no")
-      setRemoveBlock(true)
+    if (isCorect === true) {
+      console.log("yes");
+    } else {
+      console.log("no");
+      setRemoveBlock(true);
     }
 
-
     document.getElementById("Question-form").reset();
-    setQuestion(questions[Math.floor(Math.random()*questions.length)])
-  }
+    setQuestion(questions[Math.floor(Math.random() * questions.length)]);
+  };
 
   return (
-    <div className="QuizPart" >{ !removeBlock ?
-      <form className="Form" id="Question-form" onSubmit={onSubmit} >
-        {question.q}
-        <div className="Answer">
-          <input type="radio" name="ans" value={question.a} />
-          {question.a}
-        </div>
-        <div className="Answer">
-          <input type="radio" name="ans" value={question.b} />
-          {question.b}
-        </div>
-        <div className="Answer">
-          <input type="radio" name="ans" value={question.c} />
-          {question.c}
-        </div>
-        <div className="Answer">
-          <input type="radio" name="ans" value={question.d} />
-          {question.d}
-        </div>
-        <input className="SubmitOk" type='submit' value='ok'/>
-      </form> : <p>remove a block</p>}
+    <div className="QuizPart">
+      {!removeBlock ? (
+        <form className="Form" id="Question-form" onSubmit={onSubmit}>
+          {question.q}
+          <div className="Answer">
+            <label className="Label" htmlFor="ra">
+              <input type="radio" name="ans" id="ra" value={question.a} />
+              {question.a}
+            </label>
+          </div>
+          <div className="Answer">
+            <label className="Label" htmlFor="rb">
+              <input type="radio" name="ans" id="rb" value={question.b} />
+              {question.b}
+            </label>
+          </div>
+          <div className="Answer">
+            <label className="Label" htmlFor="rc">
+              <input type="radio" name="ans" id="rc" value={question.c} />
+              {question.c}
+            </label>
+          </div>
+          <div className="Answer">
+            <label className="Label" htmlFor="rd">
+              <input type="radio" name="ans" id="rd" value={question.d} />
+              {question.d}
+            </label>
+          </div>
+          <input className="SubmitOk" type="submit" value="OK" />
+        </form>
+      ) : (
+        <p>remove a block</p>
+      )}
     </div>
   );
 };
